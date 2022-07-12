ca850ad106a099fb2c9c45f7dd903807479dd6d4
Marcelo Costa <marcelo.costa@sensedia.com>
2022-07-12 14:49:10 -0300

    adding collection postman

--- models/cervejas.go
+++ models/cervejas.go
@@ -6,5 +6,5 @@ type Cerveja struct {
 	gorm.Model
 	Nome        string `json:"nome"`
 	Ingrediente string `json:"ingrediente"`
-	Preco       string `json:"preco"`
+	Preco       string `json:"preco"` // mudar para int
 }
